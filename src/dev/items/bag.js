de06b9fa4195267602b467db845915fbe97f7f5f
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-11-28 18:03:15 +0100

    1. Added Pollen Bag 2. Added pollen collection from flowers

--- src/dev/items/bag.js
+++ src/dev/items/bag.js
@@ -1,8 +1,15 @@
 IDRegistry.genItemID("bag");
+IDRegistry.genItemID("pollen_bag");
 
 Item.setCategory("bag", 3);
+Item.setCategory("pollen_bag", 3);
 
-Item.createItem("bag", "Bag", "bag", 0, {
+Item.createItem("bag", "Bag", {name: "bag", meta: 0}, {
+    isTech: false,
+    stack: 64
+});
+
+Item.createItem("pollen_bag", "Pollen Bag", {name: "pollen_bag", meta: 0}, {
     isTech: false,
     stack: 64
 });
@@ -11,3 +18,16 @@ Recipes.addShaped({id: ItemID.bag, count: 1, data: 0}, /* Результат к�
 	["aba", "bcb", "bbb"], /* Форма рецепта */
 	['a', 287, 0, 'b', 415, 0]/* Ингридиенты */
 ); 
+
+Callback.addCallback("ItemUse", function(coords, item) {
+    // If player uses a stick
+    if (item.id == ItemID.bag && World.getBlockID(coords.x, coords.y, coords.z) == 37
+    && World.getBlockID(coords.x, coords.y, coords.z) == 38) {
+        Player.decreaseCarriedItem(1);
+        Player.addItemToInventory(ItemID.pollen_bag, 1);
+    } else if (item.id == ItemID.bag && World.getBlockID(coords.x, coords.y, coords.z) == 38
+    && World.getBlockData(coords.x, coords.y, coords.z) == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8) {
+        Player.decreaseCarriedItem(1);
+        Player.addItemToInventory(ItemID.pollen_bag, 1);
+    }
+});
