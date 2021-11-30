11d3c31ce1b731f2ec4c4e662378e3f2104a3b2c
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-11-30 19:26:59 +0100

    - Added Tile Entity to Dark Flower

--- src/dev/block/dark_flower.js
+++ src/dev/block/dark_flower.js
@@ -1,4 +1,10 @@
-IDRegistry.genBlockID(dark_flower);
+IDRegistry.genBlockID("dark_flower");
+
+Block.createSpecialType("Dark_Flower", {
+    solid: false,
+    rendertype: 1,
+    lightlevel: 3
+});
 
 Block.createBlock("dark_flower", [
     {
@@ -9,18 +15,36 @@ Block.createBlock("dark_flower", [
             ["planks", 0], ["planks", 0]
         ], inCreative: true
     }
-]); 
+], "Dark_Flower"); 
 
 TileEntity.registerPrototype(BlockID.dark_flower, {
     defaultValues: {
-        //
+        particleLoopTime: 45
     },
 
     tick: function() {
-        //
+        this.data.particleLoopTime--;
+
+        if(World.getBlockID(this.x + 1, this.y - 1, this.z) == 88
+        && World.getBlockID(this.x, this.y - 1, this.z + 1) == 88
+        && World.getBlockID(this.x - 1, this.y - 1, this.z) == 88
+        && World.getBlockID(this.x, this.y - 1, this.z - 1) == 88
+        && World.getBlockID(this.x + 1, this.y, this.z + 1) == 50
+        && World.getBlockID(this.x - 1, this.y, this.z + 1) == 50
+        && World.getBlockID(this.x - 1, this.y, this.z - 1) == 50
+        && World.getBlockID(this.x + 1, this.y, this.z - 1) == 50) {
+            Game.tipMessage("Еба ты смог...")
+        }
+
+        if(this.data.particleLoopTime <= 0) {
+            this.data.particleLoopTime = 45;
+            Particles.addFarParticle(20, this.x + 0.5, this.y + 0.5, this.z + 0.5, 0, 0, 0, 0);
+        }
     },
 
     click: function(id, count, data, coords) {
-        //
+        if(id == ItemID.pollen_bag) {
+            //
+        }
     }
 });
