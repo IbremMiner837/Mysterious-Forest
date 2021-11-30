e1e8f63c1cbd9966c1dda3aacd4034ae0cd72861
Ibragim Maltsagov <ibragim837.tab@gmail.com>
2021-11-30 18:24:26 +0100

    - Added Dark Flower ;)

---
+++ src/dev/block/dark_flower.js
@@ -0,0 +1,26 @@
+IDRegistry.genBlockID(dark_flower);
+
+Block.createBlock("dark_flower", [
+    {
+        name: "Dark Flower",
+        texture: [
+            ["planks", 0], ["planks", 0], 
+            ["planks", 0], ["planks", 0], 
+            ["planks", 0], ["planks", 0]
+        ], inCreative: true
+    }
+]);
+
+TileEntity.registerPrototype(BlockID.dark_flower, {
+    defaultValues: {
+        //
+    },
+
+    tick: function() {
+        //
+    },
+
+    click: function(id, count, data, coords) {
+        //
+    }
+});
